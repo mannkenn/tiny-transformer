1b6bc95295b331317b3e9e14e97a07758ac92eef
Manny Kim <emmanuelkim2004@gmail.com>
2026-05-01 19:16:15 +0000

    made flash attention optional through config

--- train.py
+++ train.py
@@ -3,35 +3,13 @@ import time
 import torch
 import csv
 import os
-import yaml
 import argparse
 import shutil
 from datetime import datetime
 from model import Transformer
+from utils import load_config, parse_config
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-
-def load_config(path: str):
-    with open(path, "r") as f:
-        cfg = yaml.safe_load(f)
-    return cfg
-
-def parse_config(cfg):
-    # cast config values to correct types
-    return {
-        "run_name": str(cfg["run_name"]),
-        "learning_rate": float(cfg["learning_rate"]),
-        "batch_size": int(cfg["batch_size"]),
-        "block_size": int(cfg["block_size"]),
-        "n_embd": int(cfg["n_embd"]),
-        "n_layers": int(cfg["n_layers"]),
-        "n_heads": int(cfg["n_heads"]),
-        "dropout": float(cfg["dropout"]),
-        "eval_interval": int(cfg["eval_interval"]),
-        "eval_iters": int(cfg["eval_iters"]),
-        "max_iters": int(cfg["max_iters"]),
-        "grad_accum_steps": int(cfg.get("grad_accum_steps", 1)), # optional grad accum
-    }
 
 # parse command line arguments for run_name, config file and optional checkpoint to resume from
 parser = argparse.ArgumentParser()
@@ -169,6 +147,7 @@ def train():
         n_layers=cfg["n_layers"],
         n_heads=cfg["n_heads"],
         dropout=cfg["dropout"],
+        use_flash_attention=cfg["use_flash_attention"],
     ).to(device)
 
     if torch.cuda.is_available():
